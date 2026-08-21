cd2b247204ade97852419d00f6397c81a635cce4
406-16-Rawisara <54424@yupparaj.ac.th>
2026-08-21 11:04:24 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -100,11 +100,11 @@ ans2 = st.text_input(
 )
 ans3 = st.text_input(
     "ข้อ 3: A `M _ n g _ `  yellow fruit. 🥭",
-    value=st.session_state.ans1_val,
+    value=st.session_state.ans3_val,
 )
 ans4 = st.text_input(
     "ข้อ 4: Monkeys like to eat  `B _ _ _ n a `. 🍌 ",
-    value=st.session_state.ans2_val,
+    value=st.session_state.ans4_val,
 )
 # อัปเดตค่าล่าสุดเข้าตัวแปร
 st.session_state.ans1_val = ans1
